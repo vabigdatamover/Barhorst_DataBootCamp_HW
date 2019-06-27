d7577a4f2ac0bdbe76b0a20acd92faa3b40e81bd
unknown <vabigdatamover@gmail.com>
2019-06-26 20:24:27 -0400

    Loading Unit 3 Homework

--- Barhorst-Homework/Unit_3_Python_Homework/PyBank.py
+++ Barhorst-Homework/Unit_3_Python_Homework/PyBank.py
@@ -13,43 +13,68 @@ csvpath = os.path.join("Resources",'budget_data.csv')
 # Open the CSV
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
-#set the output of the text file
-#text_path = "output.txt"
-    
-    print(f'Financial Analysis')
-    print(f'--------------------------------')  
- 
-def BankData(data):
-    monthes = data[0]
-    values = int(data[1])
-    
+  
     # Skip Header  
     next(csvreader, None)
     #print(f"Header: {csvreader}")
-      
-# Loop through and print to count the number of months
+    
+    print(f'Financial Analysis')
+    print(f'--------------------------------') 
+    
+    # Loop through and print to count the number of months
     month = 0
+    total = 0
+    differences = []
+    odd_row_value = 0
+    even_row_value = 0
     for row in csvreader:
         month += 1
+        total += int(row[1])
+#        if month % 2 =! 0:
+#            #SAVE THIS VALUE AS odd_row_value
+#        else:
+#            #SUBTRACT THIS ROW VALUE FROM odd_row_value
+#            #put that difference in differences
+#            
+        
+ # Loop through and print to count the number of months        
     print(f'Total Months: {month}')
+# Loop through print the net total amount of "Profit/Losses" over the entire period     
+    print(f'Total: $ {total}')
+# Loop through and print the average of the changes in "Profit/Losses" over the entire period   
+    #average_change = month.mean()*100
+    #print(f'Average Change: $ {sum(total)}')
+    
+    
+
+#set the output of the text file
+#text_path = "output.txt"
+        
+    
+#def BankData(data):
+#    monthes = data[0]
+#    values = int(data[1])
+#    
+
                     
-# Loop through print the net total amount of "Profit/Losses" over the entire period
-    profit_losses = sum(values)*100
-    print(f'Total: $ {profit_losses}%')
+## Loop through print the net total amount of "Profit/Losses" over the entire period
+#    profit_losses = sum(values)*100
+#    print(f'Total: $ {profit_losses}%')
            
 # Loop through and print the average of the changes in "Profit/Losses" over the entire period
-    average_change = values.mean()*100
-    print(f'Average Change: $ {average_change}')
+#    average_change = values.mean()*100
+#    print(f'Average Change: $ {average_change}')
             
 # Loop through and print the greatest increase in profits (date and amount) over the entire period
 
-    greatest_increase = max(values)*100
-    print(f'Greatest Increase: {monthes} (${greatest_increase})')
-            
+#    greatest_increase = max(values)*100
+#    print(f'Greatest Increase: {monthes} (${greatest_increase})')
+#            
 # Loop through and print the the greatest decrease in losses (date and amount) over the entire period
-    greatest_decrease = min(values)*100
-    print(f'Greatest Decrease of Profits: {monthes} (${greatest_decrease})')
+#    greatest_decrease = min(values)*100
+#    print(f'Greatest Decrease of Profits: {monthes} (${greatest_decrease})')
+    
+#    return 
           
 # Specify the file to write to
 output_path = os.path.join("Resources",'PyBank_Export.csv')
